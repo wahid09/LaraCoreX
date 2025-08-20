6dea71fcff20509d363091acd60ddca3da7ae31c
wahid ullah <wahid.ullah.cse@gmail.com>
2025-08-20 22:22:25 +0600

    Some package added

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::middleware(['auth'])->group(function () {
-    // Route::get('/home', function () {
-    //     return view('home');
-    // })->name('home');
     Route::get('/', function () {
     return view('dashboard');
     });
